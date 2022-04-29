ea877dfdfc0129b7f7e1189a9a3b2b18aa16fce3
Mohammad Arham Imam <imamarham10@gmail.com>
2022-04-29 11:16:26 +0530

    footer changed

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './footer.css';
 const Footer = () => {
-  return <footer>Made with <i class="fi fi-sr-heart"></i> by Arham</footer>
+  return <footer>Made with <i class="fi fi-sr-heart"></i> by Arham Imam</footer>
 }
 
 export default Footer;
