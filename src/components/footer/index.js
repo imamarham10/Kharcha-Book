8660b103609cb2631220c9c17519380bc55e5204
Mohammad Arham Imam <imamarham10@gmail.com>
2022-04-30 15:18:38 +0530

    added heart image in footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './footer.css';
 const Footer = () => {
-  return <footer>Made with <i class="fi fi-sr-heart"></i> by Arham Imam</footer>
+  return <footer>Made with <img src={require('../../resources/heart.png')} alt="love"></img> by Arham Imam</footer>
 }
 
 export default Footer;
